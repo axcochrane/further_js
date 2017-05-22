9c47f279f8aebf6d15916d7b8f6c526096733eef
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-22 17:33:52 +0100

    working toEquals test, incomplete arrayToContain test

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -1,24 +1,23 @@
-// .toEqual
-
-// describe ('Class', function(){
-//   it ('does a certain action',function(){
-//     expect(test_function()). toEqual () // toBeTrue
-//   });
-// });
-//
-//
-// expect
-// toEqual
-
 (function(describe) {
-  function testEqualsArgument(arguement) {
-    var test_function = new Test_function();
 
-    if (test_function !== arguement) {
-      throw new Error("Does not equal");
+  function toEq(test_name, expected_outcome, test_function) {
+    if (test_function === expected_outcome) {
+      document.write (test_name + " - Pass");
     } else {
-      return "Equals";
+      document.write (test_name + " - Fail");
     }
   }
-    testEqualsArgument("true");
+
+  function arrayToContain(test_name, element, array) {
+    if (array.find["element_1"] === true)  {
+      document.write (test_name + " - Pass");
+    } else {
+      document.write (test_name + " - Fail");
+    }
+  }
+
+
+    toEq("returns true value", "true", test_function());
+    arrayToContain("array contain element", "test_element", ["element_1", "element_2", "test_element"]);
+
 })(this);
--- src/notes.js
+++ src/notes.js
@@ -1,6 +1,10 @@
-(function(describe) {
-  function Test_function() {
-    return "true";
-  }
-  describe.Test_function = Test_function;
-})(this);
+// (function(describe) {
+//   function Test_function() {
+//     return "true";
+//   }
+//   describe.Test_function = Test_function;
+// })(this);
+
+function test_function(){
+  return "true"
+};
